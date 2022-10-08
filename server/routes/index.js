b030af53d8bef5e715cea0978807178ac213d88e
Chihiro Hasegawa <1114chihiro@gmail.com>
2022-10-08 19:54:58 -0400

    created Update.ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -17,32 +17,18 @@ router.get('/', function (req, res, next) {
 
 // get home page
 router.get('/home', function (req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('login', { title: 'Home' });
 });
 
-// get About page
-router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'About Me' });
-});
-
-// get Projects page
-router.get('/Projects', function (req, res, next) {
-  res.render('Projects', { title: 'Projects' });
-});
-
-// get Services page
-router.get('/Services', function (req, res, next) {
-  res.render('Services', { title: 'Services' });
-});
-
-// get Contact page
-router.get('/Contact', function (req, res, next) {
-  res.render('Contact', { title: 'Contact' });
-});
-
-// get Login page
+// get BusinessContactList page
 router.get('/BusinessContactList', function (req, res, next) {
   res.render('BusinessContactList', { title: 'Business Contact List' });
 });
+
+// get Update page
+router.get('/Update', function (req, res, next) {
+  res.render('Update', { title: 'Update' });
+});
+
 
 module.exports = router;
