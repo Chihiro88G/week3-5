e4c1605f855ee38c3d5c3cf810282b6c86830f96
Chihiro Hasegawa <1114chihiro@gmail.com>
2022-10-23 20:38:09 -0400

    updated URI for mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,7 @@
 
 // set URI to use book_store
 module.exports = {
-  "URI": "mongodb://127.0.0.1:27017/users",
+  // "URI": "mongodb://127.0.0.1:27017/users",
+  "URI": "mongodb+srv://chihiro:UvLzpldvBOJuWGak@mongodbserver.utqfcxo.mongodb.net/user_collection?retryWrites=true&w=majority",
   "Secret": "SomeSecret"
 }
